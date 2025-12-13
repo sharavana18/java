7f74c1d6581dce18af4c2e86dfc775b6f4c3925b
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-13 09:33:14 +0530

    Delete Blood.java

--- Blood.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Blood{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String b = sc.nextLine();
-        System.out.println("blood = "+b);
-
-    }
-}
