b994181ed3b34d4bb9460ddff632cc00a996bbd8
sharavana18 <sharavanakumar2020@gmail.com>
2025-12-13 09:35:02 +0530

    Delete Tabspacebetween.java

--- Tabspacebetween.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Tabspacebetween{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.printf("\t%d"+"\t%d",a,b);
-    }
-}
